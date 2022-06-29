e28b0b5b6d31eab3a697261d2ae81c0f86bfcab2
sayhith <adrialwin@gmail.com>
2022-06-28 21:55:52 -0700

    enable rock, paper, scissors buttons

--- scripts.js
+++ scripts.js
@@ -1,85 +1,94 @@
-const rock = 'rock';
-const paper = 'paper';
-const scissors = 'scissors';
+// const rock = 'rock';
+// const paper = 'paper';
+// const scissors = 'scissors';
 
-let gameMoves = [rock, paper, scissors];
+let gameMoves = ['rock', 'paper', 'scissors'];
 
 function computerPlay() {
     return gameMoves[Math.floor(Math.random() * gameMoves.length)];
 }
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
+        console.log("You tie!");
         return null;
     }
 
     switch (playerSelection) {
         case "rock":
-            if (computerSelection === scissors) {
+            if (computerSelection === "scissors") {
+                console.log("You win!");
                 return true;
             }
-            if (computerSelection === paper) {
+            if (computerSelection === "paper") {
+                console.log("You lose!");
                 return false;
             }
             break;
         case "paper":
-            if (computerSelection === rock) {
+            if (computerSelection === "rock") {
+                console.log("You win!");
                 return true;
             }
-            if (computerSelection === scissors) {
+            if (computerSelection === "scissors") {
+                console.log("You lose!");
                 return false;
             }
             break;
         case "scissors":
-            if (computerSelection === paper) {
+            if (computerSelection === "paper") {
+                console.log("You win!");
                 return true;
             }
-            if (computerSelection === rock) {
+            if (computerSelection === "rock") {
+                console.log("You lose!");
                 return false;
             }
     }
+
 }
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
+// function game() {
+//     let playerWins = 0;
+//     let computerWins = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let playerInput = getPlayerInput();
-        let computerInput = computerPlay();
+//     for (let i = 0; i < 5; i++) {
+//         let playerInput = getPlayerInput();
+//         let computerInput = computerPlay();
 
-        switch (playRound(playerInput, computerInput)) {
-            case true:
-                playerWins++;
-                console.log("You win!");
-                break;
-            case false:
-                computerWins++;
-                console.log("You lose!");
-                break;
-            case null:
-                console.log("You tie!");
-        }
-    }
-    determineWinner(playerWins, computerWins); 
-}
+//         switch (playRound(playerInput)) {
+//             case true:
+//                 playerWins++;
+//                 console.log("You win!");
+//                 break;
+//             case false:
+//                 computerWins++;
+//                 console.log("You lose!");
+//                 break;
+//             case null:
+//                 console.log("You tie!");
+//         }
+//     }
+//     determineWinner(playerWins, computerWins); 
+// }
 
-function getPlayerInput() {
-    playerInput = prompt("Rock, paper or scissors?").toLowerCase();
-    if (verifyInput(playerInput)) {
-        return playerInput;
-    } else {
-        console.log("Invalid entry.");
-        return getPlayerInput();
-    }
-}
+// function getPlayerInput() {
+//     let playerInput = prompt("Rock, paper or scissors?").toLowerCase();
+//     if (verifyInput(playerInput)) {
+//         return playerInput;
+//     } else {
+//         console.log("Invalid entry.");
+//         return getPlayerInput();
+//     }
+// }
 
-function verifyInput(playerInput) {
-    if ((playerInput === rock) || (playerInput === paper) || (playerInput === scissors)) {
-        return true;
-    }
-}
+// function verifyInput(playerInput) {
+//     if ((playerInput === rock) || (playerInput === paper) || (playerInput === scissors)) {
+//         return true;
+//     }
+// }
 
 function determineWinner(playerWins, computerWins) {
     switch (playerWins > computerWins) {
@@ -94,5 +103,19 @@ function determineWinner(playerWins, computerWins) {
     }
 }
 
-game();
+
+const buttons = document.querySelectorAll('.button')
+const button = buttons[0];
+console.log(button.getAttribute("id"));
+
+// onclick="alert(this.id)"
+
+
+// buttons.forEach(button => button.addEventListener("click", playRound()));
+
+// const computerInput = computerPlay();
+// const playerInput = document.querySelector();
+
+
+
 
